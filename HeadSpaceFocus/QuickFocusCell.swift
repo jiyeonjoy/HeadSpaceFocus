667d937724b808d18b11d2022632840d80c185cb
최지연 <choi02647@naver.com>
2023-08-02 17:21:43 +0900

    ui done

--- HeadSpaceFocus/QuickFocusCell.swift
+++ HeadSpaceFocus/QuickFocusCell.swift
@@ -9,4 +9,14 @@ import UIKit
 
 class QuickFocusCell: UICollectionViewCell {
     
+    @IBOutlet weak var thumbnailImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    func configure(_ quickFocus: QuickFocus) {
+        thumbnailImageView.image = UIImage(named: quickFocus.imageName)
+        
+        titleLabel.text = quickFocus.title
+        descriptionLabel.text = quickFocus.description
+    }
 }
--- HeadSpaceFocus/QuickFocusHeaderView.swift
+++ HeadSpaceFocus/QuickFocusHeaderView.swift
@@ -8,5 +8,9 @@
 import UIKit
 
 class QuickFocusHeaderView: UICollectionReusableView {
+    @IBOutlet weak var titleLabel: UILabel!
     
+    func configure(_ title: String) {
+        titleLabel.text = title
+    }
 }
